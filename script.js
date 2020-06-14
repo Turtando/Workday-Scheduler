3e53a82f344fd0ea3af2148a21ecd9ec591bb6a3
Chris Turton <christopheraturton@gmail.com>
2020-06-14 18:26:36 -0400

    updates to script

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
 // Global Vars
-const rows = document.getElementsByClassName("row");
-let timeSlots = [
+var row = document.getElementsByClassName("row");
+console.log(row)
+var timeSlots = [
     { name: "9am", value: 9 }, { name: "10am", value: 10 }, { name: "11am", value: 11 }, { name: "12pm", value: 12 },
     { name: "1pm", value: 13 }, { name: "2pm", value: 14 }, { name: "3pm", value: 15 }, { name: "4pm", value: 16 },
     { name: "5pm", value: 17 }
 ];
+console.log(timeSlots[0].value)
+console.log(timeSlots.value)
+
+// Function for present time
+let currentHour = parseInt(moment().format('H'));
+console.log(currentHour)
 
 // Date function
 var date = moment().format('MMMM Do YYYY');
 document.getElementById("live-date").innerHTML = date;
-
-// Time Function
+console.log(date)
+// Color Function
 // we will view this function as how we color "Past" "Present" "Future"
-var time = new Date().getHours();
-console.log(time)
-
-if (timeSlots.value === time) {
-    document.textContent.style.backgroundColor="red"
-} else if (timeSlots.value < time) {
+function addColor() {
 
 }
-
-
-// Array.from(timeSlots).forEach(row => {
-//     let
-//         rowIdString = row.id,
-//         rowHour;
-//     if (rowIdString) {
-//         rowHour = parseInt(rowIdString);
-//     }
-//     if (timeSlots.value === today.getHours()) {
-//         setColor(textArea, "red")
-//     }
-// })
-
-
-
-
 // // Clock Function
 // function startTime() {
-//     var today = new Date();
-//     var h = today.getHours();
+//     
+//     
 //     var m = today.getMinutes();
 //     var s = today.getSeconds();
 //     h = checkTime(h);
