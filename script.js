396281f8f71c9b3abc12b39b3bb883056e44e588
Chris Turton <christopheraturton@gmail.com>
2020-06-14 12:12:17 -0400

    update to files

---
+++ script.js
@@ -0,0 +1,20 @@
+// View Functions as "Past" "Present" "Future"
+
+// Global Var
+// var liveClock = $("#live-clock");
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('live-clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
